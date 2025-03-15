05fc0c42132d4f78783fcf98d057df84dba769d6
kollynce <collinsodeny@gmail.com>
2025-03-15 10:07:16 +0300

    Enhance deployment workflow: add predeploy and deploy scripts, update Node.js version, and streamline GitHub Actions for building and deploying to GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/Jewelry/',
+  base: '/Jewelry/', // Replace with your repository name Jewelry
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
